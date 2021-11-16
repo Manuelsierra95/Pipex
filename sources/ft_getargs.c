42308e74c6cd147cade88bac879c19a0e443efdd
Manuel Sierra munoz <msierra-@c2r6s6.42madrid.com>
2021-11-16 19:07:34 +0100

    pipex

--- sources/ft_getargs.c
+++ sources/ft_getargs.c
@@ -6,7 +6,7 @@
 /*   By: msierra- <msierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 18:10:20 by msierra-          #+#    #+#             */
-/*   Updated: 2021/11/16 17:51:45 by msierra-         ###   ########.fr       */
+/*   Updated: 2021/11/16 19:06:40 by msierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,24 @@ void	ft_getpath(t_pipex *pipex)
 
 void	ft_getcmd(char *argv, t_pipex *pipex)
 {
+	int	i;
+	int	flag;
+
+	i = 0;
+	flag = 0;
+	if (!argv)
+		ft_errormsg(-1);
+	while (argv[i])
+	{
+		if (argv[i] >= 'a' && argv[i] <= 'z')
+		{
+			flag = 1;
+			break ;
+		}
+		i++;
+	}
+	if (flag == 0)
+		ft_errormsg(-1);
 	pipex->cmd = ft_split(argv, ' ');
 }
 
